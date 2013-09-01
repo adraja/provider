ed8606d49da091b1379e863d22e6c872f258d6b6
adraja <adraja@gmail.com>
2013-09-01 10:28:57 -0700

    Moved bmi and weight to usertests table.

--- assesscustomer.php
+++ assesscustomer.php
@@ -22,7 +22,7 @@ else
 }
 $userid=$_GET["userid"];
 $usertestsid = $_GET["usertestsid"];
-$qrystr = "select firstname,lastname,email,bmi,weight from user where id=".$userid;
+$qrystr = "select firstname,lastname,email from user where id=".$userid;
 $res = $mysqli->query($qrystr);
 if ( $res && $res->num_rows > 0 )
 {
@@ -30,27 +30,29 @@ if ( $res && $res->num_rows > 0 )
   $firstname=$row[0];
   $lastname=$row[1];
   $email=$row[2];
-  $bmi=$row[3];
-  $weight=$row[4];
 
   include("customerheader.php");
 
-  $form = new Form("Custome Information");
+  $form = new Form("Screening Information");
   $form->configure(array("action" => "updatecustomer.php", "method" => "get"));
-  $form->addElement(new Element\HTML("<img src='Nutriligence.png'><h1>Customer Information</h1>"));
+  $form->addElement(new Element\HTML("<img src='Nutriligence.png'><h1>Customer Screening Information</h1>"));
   $form->addElement(new Element\Hidden("userid", $userid));
   $form->addElement(new Element\Hidden("usertestsid", $usertestsid));
   $form->addElement(new Element\TextBox("Email: ", "email", array("readonly"=>true, "value" => $email)));
-  $qrystr2 = "select score,taken, details,recommendation from usertests where id=".$usertestsid;
+  $qrystr2 = "select score,taken,bmi,weight,details,recommendation from usertests where id=".$usertestsid;
   $res2 = $mysqli->query($qrystr2);
   if ( $res2 && $res2->num_rows > 0 )
   {
       $row = $res2->fetch_row();
       $score = $row[0];
       $taken = $row[1];
-      $details = $row[2];
-      $recommendation = $row[3];
+      $bmi = $row[2];
+      $weight = $row[3];
+      $details = $row[4];
+      $recommendation = $row[5];
       $form->addElement(new Element\TextBox("Score: ", "score", array("value" => $score)));
+      $form->addElement(new Element\TextBox("BMI: ", "bmi", array("value" => $bmi)));
+      $form->addElement(new Element\TextBox("Weight: ", "weight", array("value" => $weight)));
       $form->addElement(new Element\TextArea("Details: ", "details", array("value" => $details)));
       $form->addElement(new Element\TextArea("Recommendation: ", "recommendation", array("value" => $recommendation)));
   }
@@ -71,24 +73,24 @@ if ( $res && $res->num_rows > 0 )
   if ( $res3 && $res3->num_rows > 0 )
   {
  
-  $num_rows = $res3->num_rows;
-  while ( $num_rows > 0 )
-  {
-    $user = $res->fetch_row();
-    $id = $user[0];
-    $score = $user[1];
-    $taken = $user[2];
+    $num_rows = $res3->num_rows;
+    while ( $num_rows > 0 )
+    {
+      $user = $res->fetch_row();
+      $id = $user[0];
+      $score = $user[1];
+      $taken = $user[2];
   
-    echo "  <tr>\n";
-    echo "    <td>".$id."</td>\n";
-    echo "    <td>".$score."</td>\n";
-    echo "    <td>".substr($taken,0,10)."</td>\n";
-    echo "    <td><a href='whatdoesitmean.php?userid=".$id."&usertestsid=".$usertestsid."'>View Details</a></td>\n";
-    echo "    <td><a href='howtoimprove.php?userid=".$id."&usertestsid=".$usertestsid."'>View Details</a></td>\n";
-    echo "  </tr>\n";
-    $num_rows = $num_rows-1;
+      echo "  <tr>\n";
+      echo "    <td>".$id."</td>\n";
+      echo "    <td>".$score."</td>\n";
+      echo "    <td>".substr($taken,0,10)."</td>\n";
+      echo "    <td><a href='whatdoesitmean.php?userid=".$id."&usertestsid=".$usertestsid."'>View Details</a></td>\n";
+      echo "    <td><a href='howtoimprove.php?userid=".$id."&usertestsid=".$usertestsid."'>View Details</a></td>\n";
+      echo "  </tr>\n";
+      $num_rows = $num_rows-1;
+    }
   }
-}
 
   include("customerfooter.php");
 }
