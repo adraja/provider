64233c83795fbed207cd74eab1abdb8d75dc66b7
adraja <adraja@gmail.com>
2013-09-01 10:31:24 -0700

    Removed Find user tab.

--- providerheader.php
+++ providerheader.php
@@ -11,8 +11,7 @@ echo "<table border=\"1\" cellpadding=\"0\" cellspacing=\"0\">\n";
   echo "    <h2>MENU</h2>\n";
   echo "              <a href=\"/provider/provideraccountsettings.php?contactemail=".$contactemail."&password=".$password."\">Account Settings</a><br/><p><p>\n";
   echo "              <a href=\"/provider/customer.php?contactemail=".$contactemail."&password=".$password."\">Customer</a><br/><p>\n";
-  echo "              <a href=\"/provider/finduser.php?contactemail=".$contactemail."&password=".$password."\">Find User</a><p><br />\n";
   echo "            <br /><br />";
   echo "    </td>\n";
   echo "    <td width=\"600\" height=\"800\" class=\"content\">\n";
-  ?>
+?>
