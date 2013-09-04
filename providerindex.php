9d64a381aa9f12b6a3d1ce74277166f24117bdb0
adraja <adraja@gmail.com>
2013-09-03 19:04:26 -0700

    Contact to get Access goes directly to Register.php.

--- providerindex.php
+++ providerindex.php
@@ -8,7 +8,7 @@ use PFBC\Element;
 include("PFBC/Form.php");
 $form = new Form("login");
 $form->configure(array("action" => "providerlogin.php", "method" => "get"));
-$form->addElement(new Element\HTML("<img src='Nutriligence.png' height='160'>/><h3>Login to NufitScan Provider</h3><a href='contacttogetaccess.php' style='text-decoration:underline'>Contact to get access</a>"));
+$form->addElement(new Element\HTML("<img src='Nutriligence.png' height='160'>/><h3>Login to NufitScan Provider</h3><a href='Register.php' style='text-decoration:underline'>Contact to get access</a>"));
 $form->addElement(new Element\Hidden("form", "login"));
 $form->addElement(new Element\Email("Contact Email:", "contactemail", array(
 	"required" => 1
